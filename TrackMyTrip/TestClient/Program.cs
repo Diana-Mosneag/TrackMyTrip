9a23261e567f4d9df4989b9e2678120aa5b0af94
Diana-Mosneag <diana.mosneag@live.com>
2013-12-25 21:29:43 +0200

    Test Client created

---
+++ TrackMyTrip/TestClient/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using TestClient.ServiceReference1;
+
+namespace TestClient
+{
+    class Program
+    {
+        static void Main()
+        {
+            Service1Client client=new Service1Client();
+
+            Console.WriteLine(client.GetData(int.Parse(Console.ReadLine())));
+
+            client.Close();
+        }
+    }
+}
