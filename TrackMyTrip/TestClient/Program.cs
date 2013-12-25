232bc5bd30e0bc9dcf6d9098c6bb1e61d3ae15f2
Diana-Mosneag <diana.mosneag@live.com>
2013-12-25 21:31:21 +0200

    Test change

--- TrackMyTrip/TestClient/Program.cs
+++ TrackMyTrip/TestClient/Program.cs
@@ -12,6 +12,8 @@ namespace TestClient
             Console.WriteLine(client.GetData(int.Parse(Console.ReadLine())));
 
             client.Close();
+
+            Console.ReadLine();
         }
     }
 }
